30b99dced667f217f818e827b8ba753f9bedd2f7
shivam907 <shivamkaushal907@gmail.com>
2020-08-24 23:50:31 +0530

    2nd CODE

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout<<"HELLlllO WORLD";
+    int a=12, b=10, z;
+    float f=3.5;
+    char c='A';
+    double d=3.56798;
+    cout<<"HELLlllO WORLD"<<endl;
+    cout<<a<<endl;
+    cin>>a;
+    cout<<a<<" "<<b<<" "<<f;
     return 0;
 }
