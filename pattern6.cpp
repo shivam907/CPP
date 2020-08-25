33bd9ae4e084a24e93567b30c28a736d562f599a
shivam907 <shivamkaushal907@gmail.com>
2020-08-26 02:07:48 +0530

    11th Code

---
+++ pattern6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=1;
+    for(int i=1; i<=5; i++)
+    {
+        // for(int j=5; j>=i;j--)
+        // {
+        //     cout<<" ";
+        // }
+        for(int k=1; k<=i; k++)
+        {
+            
+            
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+    }
+}
